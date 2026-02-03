0f4df7ea4d67424df479b22053a03ce93a1bc6d2
Samer Ghazouani <samer.ghazouani@gmail.com>
2026-02-03 12:36:36 +0100

    Test 9

---
+++ BearAndBigBrother.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, y = 0;
+
+    cin >> a;
+    cin >> b;
+
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        y++;
+    }
+
+    cout << y;
+
+    return 0;
+}
